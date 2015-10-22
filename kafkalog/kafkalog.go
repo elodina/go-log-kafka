c318e3d83bfc0885a870fe70036fe19dc1468336
James Oliver <james.oliver@stealth.ly>
2015-10-22 08:59:36 -0700

    read from metadata chan

--- kafkalog/kafkalog.go
+++ kafkalog/kafkalog.go
@@ -3,6 +3,7 @@ package kafkalog
 import (
   "github.com/stealthly/siesta"
   "time"
+  "log"
 )
 
 func New(brokerlist []string, topic string) *Logger {
@@ -14,6 +15,8 @@ func New(brokerlist []string, topic string) *Logger {
   }
   config := siesta.NewProducerConfig()
   config.BrokerList = brokerlist
+  config.BatchSize = 10
+  config.RequiredAcks = 1
   producer := siesta.NewKafkaProducer(config, siesta.ByteSerializer, siesta.ByteSerializer, connector)
 
   return &Logger{
@@ -31,6 +34,17 @@ func (l *Logger) Write(p []byte) (n int, err error) {
   local := make([]byte, len(p), len(p))
   copy(local, p)
   l.producer.Send(&siesta.ProducerRecord{Topic: l.topic, Value: local})
+  go func() {
+    select {
+    case metadata := <-l.producer.RecordsMetadata:
+      if metadata.Error != siesta.ErrNoError {
+        log.Fatal(metadata.Error)
+      }
+    case <-time.After(5 * time.Second):
+      log.Fatal("Could not get produce response within 5 seconds")
+    }
+  }()
+
   return len(p), nil
 }
 
