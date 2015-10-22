2f22bb52b65edf95dfc5f4694e0067bd3af9daac
James Oliver <james.oliver@stealth.ly>
2015-10-22 08:59:29 -0700

    profiling

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
   "github.com/elodina/go-log-kafka/kafkalog"
+  _ "net/http/pprof"
+  "net/http"
   "log"
   "io"
   "os"
 )
 
 func main() {
-  klog := kafkalog.New([]string{"docker:9092"}, "log")
+  go profileServer()
+
+  klog := kafkalog.New([]string{"localhost:9092"}, "log")
   defer klog.Close(5000)
 
   logger := log.New(io.MultiWriter(os.Stdout, klog), "KLOG: ", log.Ldate|log.Ltime|log.Lshortfile)
   for i := 0; i < 1000; i++ {
     logger.Printf("Test message %d", i)
   }
+}
+
+func profileServer() {
+  log.Println(http.ListenAndServe("localhost:6060", nil))
 }
